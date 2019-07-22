3e29af280ed8b370440eec23d196e0e3c1147a4f
shishir75 <shishir.srdr16@gmail.com>
2019-07-23 01:01:26 +0600

    Middleware & DashboardController of All users

---
+++ app/Http/Controllers/Dept-Office/DashboardController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Dept-Office;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class DashboardController extends Controller
+{
+    //
+}
---
+++ app/Http/Controllers/VC-Office/DashboardController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\VC-Office;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class DashboardController extends Controller
+{
+    //
+}
