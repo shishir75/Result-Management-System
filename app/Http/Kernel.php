c4074888990e19812b401ebe4d104bf3c821e374
shishir75 <shishir.srdr16@gmail.com>
2019-07-23 01:05:44 +0600

    Register Middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,6 +2,11 @@
 
 namespace App\Http;
 
+use App\Http\Middleware\DeptOfficeMiddleware;
+use App\Http\Middleware\ExamControllerMiddleware;
+use App\Http\Middleware\StudentMiddleware;
+use App\Http\Middleware\TeacherMiddleware;
+use App\Http\Middleware\VcOfficeMiddleware;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -60,6 +65,11 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'vcOffice' => VcOfficeMiddleware::class,
+        'examController' => ExamControllerMiddleware::class,
+        'deptOffice' => DeptOfficeMiddleware::class,
+        'teacher' => TeacherMiddleware::class,
+        'student' => StudentMiddleware::class,
     ];
 
     /**
