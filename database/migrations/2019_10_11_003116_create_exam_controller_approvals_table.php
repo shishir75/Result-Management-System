7a496215e41f143dc78b3735bf3e63005a880b13
shishir75 <shishir.srdr16@gmail.com>
2019-10-11 00:41:05 +0600

    Exam Controller Approval Migration

--- database/migrations/2019_10_11_003116_create_exam_controller_approvals_table.php
+++ database/migrations/2019_10_11_003116_create_exam_controller_approvals_table.php
@@ -15,7 +15,13 @@ class CreateExamControllerApprovalsTable extends Migration
     {
         Schema::create('exam_controller_approvals', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->unsignedBigInteger('dept_id');
+            $table->unsignedBigInteger('session_id');
+            $table->unsignedBigInteger('year_semester_id');
+            $table->boolean('status')->default(0);
             $table->timestamps();
+            $table->foreign('session_id')->references('id')->on('sessions')->onDelete('cascade');
+            $table->foreign('dept_id')->references('id')->on('depts')->onDelete('cascade');
         });
     }
 
