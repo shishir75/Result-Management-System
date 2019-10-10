5e690a4c9b7989b482152a6f8904196f9acedb7f
shishir75 <shishir.srdr16@gmail.com>
2019-10-11 03:40:14 +0600

    Exam Controller Result Bug Fixed

--- resources/views/exam_controller/result.blade.php
+++ resources/views/exam_controller/result.blade.php
@@ -37,7 +37,12 @@
                             <div class="card-header">
                                 <h3 class="card-title">
                                     {{ strtoupper('Result of '. $semester->name ) }}
-                                    <a target="_blank" href="{{ route('exam_controller.marks.download', [$students[0]->dept->slug ,$session->id, $year->id, $semester->id]) }}" class="btn btn-sm btn-info text-white ml-5 float-right">Download PDF</a>
+                                    @if(isset($check_approval) && $check_approval->status == 1)
+                                        <a target="_blank" href="{{ route('exam_controller.marks.download', [$students[0]->dept->slug ,$session->id, $year->id, $semester->id]) }}" class="btn btn-sm btn-info text-white ml-5 float-right">Download PDF</a>
+                                        <span class="btn btn-sm btn-success float-right">Approved</span>
+                                    @else
+                                        <span class="btn btn-sm btn-danger float-right">Not Approved</span>
+                                    @endif
                                     <span class="ml-5">SESSION : {{ $session->name }}</span>
                                 </h3>
                             </div>
