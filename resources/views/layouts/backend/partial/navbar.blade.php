d5da5019748d2f047f454b276caaea5a485613b8
shishir75 <shishir.srdr16@gmail.com>
2019-10-11 04:12:32 +0600

    Admin Panel Clean Up

--- resources/views/layouts/backend/partial/navbar.blade.php
+++ resources/views/layouts/backend/partial/navbar.blade.php
@@ -4,5 +4,10 @@
 		<li class="nav-item">
 			<a class="nav-link" data-widget="pushmenu" href="#"><i class="fa fa-bars"></i></a>
 		</li>
+        <li class="nav-item">
+            <h4 class="mt-2">Result Management System (RMS)</h4>
+        </li>
 	</ul>
 </nav>
+
+
--- resources/views/layouts/backend/partial/sidebar.blade.php
+++ resources/views/layouts/backend/partial/sidebar.blade.php
@@ -1,7 +1,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
 	<!-- Brand Logo -->
 	<a href="index3.html" class="brand-link">
-		<img src="{{ asset('assets/backend/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+		<img src="{{ asset('assets/backend/img/logo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
 		     style="opacity: .8">
 		<span class="brand-text font-weight-light">RMS</span>
 	</a>
